ea7c0662113dcd01343dbd63a3df54da7181dcd1
AliqueIvery <62438038+AliqueIvery@users.noreply.github.com>
2021-03-11 10:33:36 -0500

    Update PostsDAO.java
    added imports

--- PostsDAO.java
+++ PostsDAO.java
@@ -1,6 +1,8 @@
 package com.revature.data;
 
 import org.springframework.stereotype.Repository;
+import com.revature.beans.Posts;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
 public interface PostsDAO extends JpaRepository<Posts, Integer> {
