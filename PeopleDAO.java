267c2de1311cde2cde30b71ea5a6e72032632a3a
AliqueIvery <62438038+AliqueIvery@users.noreply.github.com>
2021-03-11 10:27:22 -0500

    Update PeopleDAO.java
    added imports

--- PeopleDAO.java
+++ PeopleDAO.java
@@ -1,6 +1,8 @@
 package com.revature.data;
 
 import org.springframework.stereotype.Repository;
+import com.revature.beans.People;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
 public interface PeopleDAO  extends JpaRepository<People , Integer>{
