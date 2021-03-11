7c7b862ed4e0e8d562fbf1c227a654b95d2840ba
AliqueIvery <62438038+AliqueIvery@users.noreply.github.com>
2021-03-11 10:35:17 -0500

    Update RolesDAO.java
    added imports

--- RolesDAO.java
+++ RolesDAO.java
@@ -1,8 +1,11 @@
 package com.revature.data;
 
 import org.springframework.stereotype.Repository;
+import com.revature.beans.Roles;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
 public interface RolesDAO extends JpaRepository<Roles , Integer> {
 
 }
+
