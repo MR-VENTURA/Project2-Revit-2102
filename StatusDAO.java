ae91678da37749cbd8b99bd97fe46b137df873d9
AliqueIvery <62438038+AliqueIvery@users.noreply.github.com>
2021-03-11 10:24:59 -0500

    Update StatusDAO.java
    added imports

--- StatusDAO.java
+++ StatusDAO.java
@@ -1,6 +1,8 @@
 package com.revature.data;
 
 import org.springframework.stereotype.Repository;
+import com.revature.beans.Status;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
 public interface StatusDAO extends JpaRepository<Status , Integer> {
