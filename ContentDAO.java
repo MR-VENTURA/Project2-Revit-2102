028b6577bfcc58d2e4f215a8d227d81df56b009e
AliqueIvery <62438038+AliqueIvery@users.noreply.github.com>
2021-03-11 10:26:04 -0500

    Update ContentDAO.java
    added imports

--- ContentDAO.java
+++ ContentDAO.java
@@ -1,8 +1,11 @@
 package com.revature.data;
 
 import org.springframework.stereotype.Repository;
+import com.revature.beans.Content;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
 public interface ContentDAO extends JpaRepository<Content , Integer> {
 
 }
+
