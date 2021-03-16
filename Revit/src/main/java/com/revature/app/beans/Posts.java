2ce3dbb1eedfe17a68a9aca327bd640e167a14ec
MRVentura <milo.r.v@gmail.com>
2021-03-16 09:11:05 -0600

    backend working for get post by id

---
+++ Revit/src/main/java/com/revature/app/beans/Posts.java
@@ -0,0 +1,144 @@
+package com.revature.app.beans;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Posts {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private Integer postId;
+	@Column(name="author_id")
+	private Integer authorId;
+	@Column(name="parent_post_id")
+	private Integer parentPostId;
+	@Column(name="flagged_for_review")
+	private Boolean flaggedForReview;
+	private Integer likes;
+	private Integer dislikes;
+	@Column(name="last_activity_date")
+	private Long lastActivityDate;
+	
+	public Posts() {
+		this.authorId = 0;
+		this.dislikes = 0;
+		this.likes = 0;
+		this.flaggedForReview = false;
+		this.lastActivityDate = null;
+		this.parentPostId = 0;
+		this.postId = 0;
+
+	}
+	public Integer getPostId() {
+		return postId;
+	}
+	public void setPostId(Integer postId) {
+		this.postId = postId;
+	}
+	public Integer getAuthorId() {
+		return authorId;
+	}
+	public void setAuthorId(Integer authorId) {
+		this.authorId = authorId;
+	}
+	public Integer getParentPostId() {
+		return parentPostId;
+	}
+	public void setParentPostId(Integer parentPostId) {
+		this.parentPostId = parentPostId;
+	}
+	public Boolean getFlaggedForReview() {
+		return flaggedForReview;
+	}
+	public void setFlaggedForReview(Boolean flaggedForReview) {
+		this.flaggedForReview = flaggedForReview;
+	}
+	public Integer getLikes() {
+		return likes;
+	}
+	public void setLikes(Integer likes) {
+		this.likes = likes;
+	}
+	public Integer getDislikes() {
+		return dislikes;
+	}
+	public void setDislikes(Integer dislikes) {
+		this.dislikes = dislikes;
+	}
+	public Long getLastActivityDate() {
+		return lastActivityDate;
+	}
+	public void setLastActivityDate(Long lastActivityDate) {
+		this.lastActivityDate = lastActivityDate;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((authorId == null) ? 0 : authorId.hashCode());
+		result = prime * result + ((dislikes == null) ? 0 : dislikes.hashCode());
+		result = prime * result + ((flaggedForReview == null) ? 0 : flaggedForReview.hashCode());
+		result = prime * result + ((lastActivityDate == null) ? 0 : lastActivityDate.hashCode());
+		result = prime * result + ((likes == null) ? 0 : likes.hashCode());
+		result = prime * result + ((parentPostId == null) ? 0 : parentPostId.hashCode());
+		result = prime * result + ((postId == null) ? 0 : postId.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Posts other = (Posts) obj;
+		if (authorId == null) {
+			if (other.authorId != null)
+				return false;
+		} else if (!authorId.equals(other.authorId))
+			return false;
+		if (dislikes == null) {
+			if (other.dislikes != null)
+				return false;
+		} else if (!dislikes.equals(other.dislikes))
+			return false;
+		if (flaggedForReview == null) {
+			if (other.flaggedForReview != null)
+				return false;
+		} else if (!flaggedForReview.equals(other.flaggedForReview))
+			return false;
+		if (lastActivityDate == null) {
+			if (other.lastActivityDate != null)
+				return false;
+		} else if (!lastActivityDate.equals(other.lastActivityDate))
+			return false;
+		if (likes == null) {
+			if (other.likes != null)
+				return false;
+		} else if (!likes.equals(other.likes))
+			return false;
+		if (parentPostId == null) {
+			if (other.parentPostId != null)
+				return false;
+		} else if (!parentPostId.equals(other.parentPostId))
+			return false;
+		if (postId == null) {
+			if (other.postId != null)
+				return false;
+		} else if (!postId.equals(other.postId))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Posts [postId=" + postId + ", authorId=" + authorId + ", parentPostId=" + parentPostId
+				+ ", flaggedForReview=" + flaggedForReview + ", likes=" + likes + ", dislikes=" + dislikes
+				+ ", lastActivityDate=" + lastActivityDate + "]";
+	}
+	
+}
---
+++ Revit/src/main/java/com/revature/app/services/PostsService.java
@@ -0,0 +1,20 @@
+package com.revature.app.services;
+
+import java.util.Set;
+
+import com.revature.app.beans.People;
+import com.revature.app.beans.Posts;
+import com.revature.app.exceptions.PersonNotFoundException;
+import com.revature.app.exceptions.PostNotFoundException;
+
+public interface PostsService {
+	
+	//create
+	public Integer addPosts(Posts ps);
+	//read
+	public Posts findByPostId(Integer id) throws PostNotFoundException;
+	public People getPeopleByPostId(Integer id) throws PersonNotFoundException, PostNotFoundException;
+	//update
+	public void updatePosts(Posts ps)throws PostNotFoundException;
+	public Set<Posts> getAllPosts();
+}
